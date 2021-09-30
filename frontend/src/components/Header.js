03e11728b823610ae554bd63798813f542831aab
Alex <alex.soderberg97@gmail.com>
2021-09-30 10:50:25 +0200

    added a component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,34 +3,34 @@ import {Container, Row, Nav, Col, List} from 'react-bootstrap';
 
 function Header() {
     return(
-    <Container>
-        <Nav>
-            <Row>
-                <Col>1</Col>
-                <Col>2</Col>
-                <Col>3</Col>
-            </Row>
-        </Nav>
-    </Container>
-    // <nav className="nav navbar-light bg-light ">
-    //     <ol className="d-flex flex-row justify-content-md-center">
-    //         <li className="nav-item">
-    //             <a className="nav-link">
-    //                 insDocs
-    //             </a>
-    //         </li>
-    //         <li className="nav-item">
-    //             <a className="nav-link">
-    //                 Finished
-    //             </a>
-    //         </li>
-    //         <li className="nav-item">
-    //             <a className="nav-link">
-    //                 new
-    //             </a>
-    //         </li>
-    //     </ol>
-    // </nav>
+    // <Container>
+    //     <Nav>
+    //         <Row>
+    //             <Col>1</Col>
+    //             <Col>2</Col>
+    //             <Col>3</Col>
+    //         </Row>
+    //     </Nav>
+    // </Container>
+    <nav className="nav navbar-light bg-light ">
+        <ol className="d-flex flex-row justify-content-md-center">
+            <li className="nav-item">
+                <a className="nav-link">
+                    insDocs
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link">
+                    Finished
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link">
+                    new
+                </a>
+            </li>
+        </ol>
+    </nav>
 
     )
 }
