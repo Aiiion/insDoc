84fcb041bb1a4cdd0b9e25d0b7a51d3b289aef7f
Alex <alex.soderberg97@gmail.com>
2021-10-12 20:13:05 +0200

    adds search on templateView

--- frontend/src/modules/views/TemplateView.js
+++ frontend/src/modules/views/TemplateView.js
@@ -3,40 +3,58 @@ import PropTypes from "prop-types";
 import TemplateIcon from "../components/TemplateIcon";
 import Form from "react-bootstrap/Form";
 
-async function getTemplates(user_id) {
-  console.log("getting Templates " + user_id);
-  return fetch(`http://localhost:8080/api/doc/docTemplates/${user_id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((data) => data.json());
-}
-
 function TemplateView({ user }) {
   const [templates, setTemplates] = useState([]);
   const [searchParams, setSearchParams] = useState("");
+  const [filteredTemplates, setFilteredTemplates] = useState([]);
+  const user_id = user.id;
 
+  const getTemplates = async(user_id) => {
+    console.log("getting Templates " + user_id);
+    const response = await fetch(`http://localhost:8080/api/doc/docTemplates/${user_id}`);
+    
+    const data = await response.json();
+    setTemplates(data)
+    setFilteredTemplates(data)
+  }
+ 
   useEffect(() => {
-    getTemplates(user.id).then((data) => setTemplates(data));
+    getTemplates(user_id)
   }, []);
+
+  const filter = (e) => {
+    const keyWord = e.target.value;
+    console.log(e.target.value)
+    console.log(templates)
+    if (keyWord !== '') {
+      const result = templates.filter((template) => {
+        return template.title
+          .toLowerCase()
+          .startsWith(keyWord.toLowerCase());
+      });
+      setFilteredTemplates(result);
+    } else {
+      setFilteredTemplates(templates);
+      console.log("reset")
+    }
+    setSearchParams(keyWord);
+  };
 
   return (
     <div className="body">
-      <Form className="mt-3">
-      <Form.Control
-            autoFocus
-            placeholder="search here..."
-            type="searchParams"
-            value={searchParams}
-            onChange={(e) => setSearchParams(e.target.value)}
-          />
-      </Form>
+      
+        <Form.Control
+          className="mt-3"
+          autoFocus
+          placeholder="search here..."
+          type="searchParams"
+          value={searchParams}
+          onChange={filter}
+        />
+      
       <h1 className="textCenter mt-3">Your insDocs</h1>
       <hr />
-      
-
-      <TemplateIcon templates={templates} />
+      <TemplateIcon templates={filteredTemplates} />
     </div>
   );
 }
